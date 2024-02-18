ee3b686b8104795bb943191e2a9e3327bc20ce71
Maidzolaola <maidzola@hijra.id>
2024-02-18 17:48:22 +0700

    commit 1

---
+++ testExplorer/apiServer/apiObjects.js
@@ -0,0 +1,77 @@
+const request = require("supertest"); // import supertest
+const { expect } = require("chai"); // import chai
+const baseUrl = require("../globalVariable/baseUrl");
+
+const url = baseUrl; //define global variable
+  
+
+async function postMethod() {
+  const response = await request(url)
+    .post("/objects")
+    .send({
+      name: "Maidzola",
+      data: {
+        Generation: "2",
+        Price: "700000",
+        Capacity: "1TB",
+      },
+    });
+  //assertation / verifikasi
+  expect(response.status).to.equal(200);
+  expect(response.body.name).to.equal("Maidzola");
+  //expect(response.data.Generation).to.equal(2);
+  //expect(response.data.Price).to.equal(700000);
+  //expect(response.data.Capacity).to.equal("1TB");
+  expect(response.body.createdAt).to.not.be.null;
+  //simpan id after POST
+  const id = response.body.id;
+  console.log("id after POST:", id);
+  return id; //Return the id
+}
+
+
+async function getMethod(id) {
+    const response = await request(url).
+    get(`/objects/${id}`)
+    //assertation / verifikasi
+    expect(response.status).to.equal(200);
+    expect(response.body.id).to.equal(id);
+    expect(response.body.name).to.equal("Maidzola");
+    // expect(response.data.Generation).to.equal(2);
+    // expect(response.data.Price).to.equal(700000);
+    //expect(response.data.Capacity).to.equal("1TB");
+    console.log(response.body);
+  }
+  
+
+async function putMethod(id) {
+    const response = await request(url)
+      .put(`/objects/${id}`)
+      .send({
+        name: "Maidzola PUT",
+        data: {
+          Generation: "2",
+          Price: "700000",
+          Capacity: "1TB",
+          Guarantee: "5",
+        },
+      });
+    //assertation / verifikasi
+    expect(response.status).to.equal(200);
+    expect(response.body.name).to.equal("Maidzola PUT");
+    // expect(response.data.Guarantee).to.equal(5);
+  }
+
+
+  async function patchMethod(id) {
+    const response = await request(url)
+      .patch(`/objects/${id}`)
+      .send({
+        name: "Maidzola PATCH"
+      });
+    //assertation / verifikasi
+    expect(response.status).to.equal(200);
+    expect(response.body.name).to.equal("Maidzola PATCH");
+  }
+
+module.exports = { postMethod, getMethod, putMethod, patchMethod };
---
+++ testExplorer/testCase/testCaseFunction_02.js
@@ -0,0 +1,19 @@
+const { postMethod, getMethod, putMethod, patchMethod } = require("../apiServer/apiObjects");
+
+//test suites
+describe("Testing API restful 2", function () {
+    let id;
+  // Test case Function 02
+  it("Test POST from Function", async function () {
+    id = await postMethod();
+  });
+  it("Test GET from Function", async function () {
+    await getMethod(id);
+  });
+  it("Test PUT from Function", async function () {
+    await putMethod(id);
+  });
+  it("Test PATCH from Function", async function () {
+    await patchMethod(id);
+  });
+});
